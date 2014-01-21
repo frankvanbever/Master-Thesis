5aeb9a549b4320c1cc85f8d3b2f5bcb51c6fa558
Frank Vanbever <frank.vanbever@gmail.com>
2014-01-21 10:38:13 +0100

    slides Thesis

---
+++ slides_thesis/auto/slides_thesis_frank.el
@@ -0,0 +1,16 @@
+(TeX-add-style-hook "slides_thesis_frank"
+ (lambda ()
+    (TeX-run-style-hooks
+     "hyperref"
+     "srcltx"
+     "beamercolorthemeann"
+     "beamerouterthememiniframes"
+     "xy"
+     "all"
+     "multicol"
+     "graphicx"
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "")))
+
